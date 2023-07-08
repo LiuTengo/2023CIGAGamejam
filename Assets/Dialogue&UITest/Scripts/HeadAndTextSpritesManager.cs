2b61956d3603b5a209c1e5f5949139a9789e5c86
Sessyoinpornhob <71918373+Sessyoinpornhob@users.noreply.github.com>
2023-07-08 12:20:21 +0800

    UI building and Sprites Solutions
    -搭建了主要的UI场景；
    -解决了中文字体的显示问题；
    -找到了Sprite图集的解决方案；
    -确定了Sprite的存储和调用方案；

---
+++ Assets/Dialogue&UITest/Scripts/HeadAndTextSpritesManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 记录所有的Sprite，并编号便于调用和生成。
+public class HeadAndTextSpritesManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
